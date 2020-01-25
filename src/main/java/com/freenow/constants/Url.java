c168283ff17fa8367a9044e141b78ba73794e32d
Saket <saket03singh@gmail.com>
2020-01-25 21:00:42 +0530

    Created endpoint for Get Users and added Url class to contain parent url

---
+++ src/main/java/com/freenow/constants/Url.java
@@ -0,0 +1,5 @@
+package com.freenow.constants;
+
+public class Url {
+    public static final String parentURL = "https://jsonplaceholder.typicode.com";
+}
---
+++ src/main/java/com/freenow/serviceEndpoints/getUsers/GetUsersEndpoint.java
@@ -0,0 +1,40 @@
+package com.freenow.serviceEndpoints.getUsers;
+
+import com.freenow.constants.Url;
+import com.freenow.utility.ServiceEndpoint;
+import com.freenow.utility.request.HttpMethod;
+import com.freenow.utility.request.Param;
+import com.freenow.utility.request.RequestBody;
+
+import java.util.List;
+
+public class GetUsersEndpoint implements ServiceEndpoint {
+
+    public GetUsersEndpoint() {
+    }
+
+    @Override
+    public String url() {
+        return String.format("%s/users", Url.parentURL);
+    }
+
+    @Override
+    public HttpMethod httpMethod() {
+        return HttpMethod.GET;
+    }
+
+    @Override
+    public List<Param> queryParameters() {
+        return null;
+    }
+
+    @Override
+    public List<Param> headers() {
+        return null;
+    }
+
+    @Override
+    public RequestBody body() {
+        return null;
+    }
+}
