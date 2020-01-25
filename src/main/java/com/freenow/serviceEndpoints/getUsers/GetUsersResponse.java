46a45b53f868db63eb856478d158582f84630955
Saket <saket03singh@gmail.com>
2020-01-25 23:10:14 +0530

    Created Response class for Get Users endpoint

---
+++ src/main/java/com/freenow/serviceEndpoints/getUsers/GetUsersResponse.java
@@ -0,0 +1,35 @@
+package com.freenow.serviceEndpoints.getUsers;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GetUsersResponse {
+    private Integer id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Address {
+        private String street;
+        private String suite;
+        private String city;
+        private String zipcode;
+        private Geo geo;
+    }
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Geo {
+        private String lat;
+        private String lng;
+    }
+}
