1def051318ef1c3834a3e2a6e75639cdb83d48a1
Saket <saket03singh@gmail.com>
2020-01-27 01:17:59 +0530

    Renamed the response class to be more explainable

--- src/main/java/com/freenow/serviceEndpoints/getUsers/User.java
+++ src/main/java/com/freenow/serviceEndpoints/getUsers/User.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class GetUsersResponse {
+public class User {
 
     private Integer id;
     private String name;
